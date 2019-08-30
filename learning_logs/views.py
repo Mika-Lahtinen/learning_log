93db3697c52c047250e5f6ef49ba27cac9d3b38b
Mika-Lahtinen <image1224@outlook.com>
2019-08-31 00:19:22 +0800

    add homepage and topics html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """Home Page"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Get topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'larning_logs/topics.html', context)
